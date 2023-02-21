0ce864c86570fae8c982122693fb96f5cf63674d
Lyubomir Dakov <80615259+Lyubomir-Dakov@users.noreply.github.com>
2023-02-21 21:09:46 +0200

    Add files via upload

--- 01. Intro to Web and Flask Framework/FlaskDemo1/main.py
+++ 01. Intro to Web and Flask Framework/FlaskDemo1/main.py
@@ -6,28 +6,64 @@ api = Api(app)
 
 
 class Book:
-    def __init__(self, title, author):
+    def __init__(self, id_, title, author):
+        self.id = id_
         self.title = title
         self.author = author
 
     def serialize(self):
-        return {"title": self.title, "author": self.author}
+        return {"id": self.id, "title": self.title, "Author": self.author}
 
 
-books = [Book(title=index, author=index) for index in range(10)]
+books = [Book(index, f'Title: {index}', f'Author: {index}') for index in range(1, 11)]
 
 
-class MyBooks(Resource):
+class BooksResources(Resource):
     def get(self):
         return {"books": [book.serialize() for book in books]}
 
     def post(self):
         data = request.get_json()
+        last_id = len(books)
+        data["id_"] = last_id + 1
         new_book = Book(**data)
         books.append(new_book)
         return new_book.serialize(), 201
 
-api.add_resource(MyBooks, '/')
+
+class BookResource(Resource):
+    def get_single_book(self, pk):
+        book = [b for b in books if b.id == pk]
+        if book:
+            return book[0]
+        return None
+
+    def get(self, pk):
+        book = self.get_single_book(pk)
+        if not book:
+            return {"message": f"Book with id - {pk} doesn't exist"}, 400
+        return book.serialize(), 200
+
+    def put(self, pk):
+        book = self.get_single_book(pk)
+        if not book:
+            return {"message": f"Book with id - {pk} doesn't exist"}, 400
+
+        data = request.get_json()
+        book.title = data['title']
+        book.author = data['author']
+        return book.serialize(), 200
+
+    def delete(self, pk):
+        book = self.get_single_book(pk)
+        if not book:
+            return {"message": f"Book with id - {pk} doesn't exist"}, 400
+        books.remove(book)
+        return {"message": "ok"}, 200
+
+
+api.add_resource(BooksResources, '/books')
+api.add_resource(BookResource, '/books/<int:pk>')
 
 if __name__ == '__main__':
     app.run(debug=True)
